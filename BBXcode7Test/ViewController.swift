1adf5060269671a4a636fd801d637d0790794d90
Christopher Stott <christopherstott@gmail.com>
2015-10-13 10:35:28 -0700

    Initial Commit

---
+++ BBXcode7Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBXcode7Test
+//
+//  Created by Chris on 2015-10-13.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
